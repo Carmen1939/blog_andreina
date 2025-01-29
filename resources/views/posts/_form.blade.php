ce4c9b382507222271ced485ad3bb3f534305b5a
andreina <andrecpj2002@gmail.com>
2025-01-29 17:53:47 -0500

    Campos en el formulario de las imagenes

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -2,20 +2,27 @@
 
 <label class="uppercase text-gray-700 text-xs">Titulo</label>
 <span class="text-xs text-red-600">@error('title') {{ $message }} @enderror</span>
-<input type="text" name="title" 
-value="{{old('title', $post->title) }}">
+<input type="text" name="title" value="{{old('title', $post->title) }}">
 <br>
 <br>
 <label class="uppercase text-gray-700 text-xs">Url</label>
 <span class="text-xs text-red-600">@error('slug') {{ $message }} @enderror</span>
-<input type="text" name="slug" 
-value="{{old('slug', $post->slug) }}">
+<input type="text" name="slug" value="{{old('slug', $post->slug) }}">
 <br>
 <br>
 <label class="uppercase text-gray-700 text-xs">ConTenido</label>
 <span class="text-xs text-red-600">@error('body') {{ $message }} @enderror</span>
 <textarea type="body" rows="5" name="body" value="">{{old('body', $post->body)}}</textarea>
-
+<br>
+<br>
+<label class="uppercase text-gray-700 text-xs">Imagen de Portda</label>
+<span class="text-xs text-red-600">@error('img_page') {{ $message }} @enderror</span>
+<input type="file" rows="5" name="img_page" value="{{old('img_page', $post->img_page)}}">
+<br>
+<br>
+<label class="uppercase text-gray-700 text-xs">Imagen de Contenido</label>
+<span class="text-xs text-red-600">@error('img_content') {{ $message }} @enderror</span>
+<input type="file" rows="5" name="img_content" value="{{old('img_content', $post->img_content)}}">
 <div>
     <a href="{{route('posts.index')}}"> Volver</a>
     <input type="submit" value="Enviar">
